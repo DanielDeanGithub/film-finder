071d04164d110ca369ac38eca8e721b8fd877fd6
DanielDeanGithub <github-new@inboxrefresh.com>
2024-05-31 16:29:01 +0100

    Add jsonResponse variable

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ const getGenres = async () => {
   try {
     const response = await fetch(urlToFetch);
     if (response.ok) {
-      
+      const jsonResponse = await response.json();
     };
   } catch (error) {
     console.log(error);
