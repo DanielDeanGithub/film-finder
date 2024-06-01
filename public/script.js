b7d0d95af09217d3fcbc22da204111c3230199b1
DanielDeanGithub <github-new@inboxrefresh.com>
2024-06-01 23:01:12 +0100

    Add jsonResponse variable

--- public/script.js
+++ public/script.js
@@ -30,7 +30,7 @@ const getMovies = async () => {
   try {
     const response = await fetch(urlToFetch);
     if (response.ok) {
-      
+      const jsonResponse = await response.json();
     }
   } catch (error) {
     console.log(error);
